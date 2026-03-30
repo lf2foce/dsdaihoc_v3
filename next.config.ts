2546ab797a8336f1ec0bb3ec34288f0d14a60077
Anh Bui <lf2foce@gmail.com>
2026-03-30 21:05:19 +0700

    help legacy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/dai-hoc/:legacyId",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
